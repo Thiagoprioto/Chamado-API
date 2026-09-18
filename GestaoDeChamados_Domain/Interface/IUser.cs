4b27e70d5981d9c98874ed30b2ccb8f067f764ef
Thiago <priotothiago@gmail.com>
2026-09-18 02:27:01 -0300

    fix: adjust structure and add domain interfaces

---
+++ GestaoDeChamados_Domain/Interface/IUser.cs
@@ -0,0 +1,12 @@
+﻿using GestaoDeChamados_Domain.Entity;
+
+
+namespace GestaoDeChamados_Domain.Interface
+{
+    public interface IUser
+    {
+        Task<User> CreateUserAsync(User user);
+        Task<User> UpdateUserAsync(User user);
+        Task<User> DeleteUserAsync(Guid id);
+    }
+}
